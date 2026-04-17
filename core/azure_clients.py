89935d5da3c17abbd75a9dd06baba33b58165945
Arif Abdullah <39575392+arifabdullah-cloud@users.noreply.github.com>
2026-04-17 18:17:55 +0800

    Replace hardcoded subscription ID with config import

--- core/azure_clients.py
+++ core/azure_clients.py
@@ -2,7 +2,7 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
 from azure.monitor.query import MetricsQueryClient
 
-SUBSCRIPTION_ID = "23ff688d-01fd-4a9f-b865-14fd60150c77"
+from core.config import AZURE_SUBSCRIPTION_ID
 
 
 def get_credential():
@@ -10,7 +10,7 @@ def get_credential():
 
 
 def get_compute_client():
-    return ComputeManagementClient(get_credential(), SUBSCRIPTION_ID)
+    return ComputeManagementClient(get_credential(), AZURE_SUBSCRIPTION_ID)
 
 
 def get_metrics_client():
